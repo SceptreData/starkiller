460579b616ae4554eb1097c70496b4f6ae98658d
David Bergeron <david.f.bergeron@gmail.com>
2017-09-18 23:28:12 -0600

    added crosshair, projectile still doesn't work BALGAD

---
+++ src/projectile.lua
@@ -0,0 +1,40 @@
+local Entity = require 'entity'
+
+local Projectile = Class('Projectile', Entity)
+
+local BULLET_SIZE = 32
+local BULLET_SPEED = 600
+
+local bullet_img = love.graphics.newImage('img/bullet.png')
+
+function Projectile:initialize(parent, origin, target)
+  Entity.initialize(self, origin.x, origin.y, BULLET_SIZE, BULLET_SIZE)
+  self.parent = parent
+  self.vel = (target - origin) * BULLET_SPEED
+  self.lifetime = 0
+end
+
+
+function Projectile:update(dt)
+  self.lifetime = self.lifetime + dt
+  local dest = self.pos + self.vel * dt
+  local x, y, cols, n_cols = Game.world:move(self, dest.x, dest.y)
+
+  for i=1, n_cols do
+    local col = cols[i]
+    if col.other ~= self.parent then
+      self:remove()
+      return
+    end
+  end
+
+  self.pos = Vec2(x, y)
+end
+
+
+function Projectile:draw()
+  local centre = self:getCentre()
+  love.graphics.draw(bullet_img, centre.x, centre.y)
+end
+
+return Projectile
